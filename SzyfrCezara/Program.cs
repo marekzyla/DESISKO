f70d0c5e143037bae148fca4c16978e3a1818a73
Marek Żyła <marekzyla95@gmail.com>
2017-11-07 16:20:21 +0100

    Szyfr Cezara

--- SzyfrCezara/Program.cs
+++ SzyfrCezara/Program.cs
@@ -8,8 +8,49 @@ namespace SzyfrCezara
 {
     class Program
     {
+        static char[] keyArray = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ".ToCharArray();
+
+        private static int flow = 3;
+
+        static string Code(string message)
+        {
+            string result = "";
+            foreach (var sign in message.ToUpper())
+            {
+                int index = GetIndex(sign) + flow;
+                if (index < keyArray.Length)
+                {
+                    result += keyArray[index];
+                }
+                else
+                {
+                    result += keyArray[index - keyArray.Length];
+                }
+            }
+            return result;
+        }
+
+
+        private static int GetIndex(char sign)
+        {
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                if (keyArray[i] == sign)
+                {
+                    return i;
+                }
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
         static void Main(string[] args)
         {
+            int keyLength = keyArray.Length;
+            string toCode = Console.ReadLine();
+
+            string codedMessage = Code(toCode);
+            Console.WriteLine(codedMessage);
         }
     }
 }
